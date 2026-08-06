57d53e13dc2b60ce5aa2542474397b1a6b7ad1b9
drajj-0011 <debrajdubey0011@gmail.com>
2026-08-06 08:30:45 +0530

    Create sum.js

---
+++ sum.js
@@ -0,0 +1,8 @@
+let a = parseInt(prompt("Enter first number:"));
+let b = parseInt(prompt("Enter second number:"));
+let c = prompt("Enter your name:");
+
+let sum = a + b;
+
+console.log("Name:", c);
+console.log("Sum =", sum);

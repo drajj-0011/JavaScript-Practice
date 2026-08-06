48fef1587685c84864b12df791b67472d32e32f2
drajj-0011 <debrajdubey0011@gmail.com>
2026-08-06 08:28:02 +0530

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,8 @@
+let name = prompt("Enter your name:");
+let college = prompt("Enter your college:");
+let course = prompt("Enter your course:");
+
+console.log("Hello, Cryptography!");
+console.log("Name:", name);
+console.log("College:", college);
+console.log("Course:", course);

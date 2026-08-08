c21dd105290ff51864795fd0c491c5194f13fa7e
drajj-0011 <debrajdubey0011@gmail.com>
2026-08-08 08:15:12 +0530

    Create prime.js

---
+++ prime.js
@@ -0,0 +1,14 @@
+let n = parseInt(prompt("Enter a number"));
+let count = 0;
+
+for (let i = 1; i <= n; i++) {
+    if (n % i == 0) {
+        count++;
+    }
+}
+
+if (count == 2) {
+    console.log("Prime");
+} else {
+    console.log("Not Prime");
+}
